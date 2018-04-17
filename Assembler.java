73d86ec37d1c8d3f8602dadbb6ff1d11a2e21048
Cynthia Valdovinos <cynthia.ireri@hotmail.com>
2018-04-17 09:19:26 -0700

    cleaning code

--- Assembler.java
+++ Assembler.java
@@ -1,6 +1,5 @@
 package Assembler;
-//Author info here
-//TODO: don't forget to document each method in all classes!
+
 import java.io.FileOutputStream;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -8,16 +7,16 @@ import java.io.PrintWriter;
 
 public class Assembler {
 	
-	// ALGORITHM:
-	// get input file name
-	// create output file name and stream
+	/* ALGORITHM:
+	Getting input file name
+	Creating output file name and stream
 	
-	// create symbol table
-	// do first pass to build symbol table (no output yet!)
-	// do second pass to output translated ASM to HACK code
+	Creating symbol table
+	First pass to build symbol table (without output yet)
+	Second pass to output translated ASM to HACK code
 	
-	// print out "done" message to user
-	// close output file stream
+	Printing out "done" message to user
+	Closing output file stream */
 	public static void main(String[] args) {
 		
 		String inputFileName, outputFileName;
@@ -25,7 +24,7 @@ public class Assembler {
 		SymbolTable symbolTable;
 		int romAddress, ramAddress;
 	
-		//get input file name from command line or console input
+		//Get input file name from command line or console input
 		if(args.length == 1) {
 			System.out.println("command line arg = " + args[0]);
 			inputFileName = args[0];
@@ -51,41 +50,29 @@ public class Assembler {
 			System.exit(0);
 		}
 		
-		// TODO: finish driver as algorithm describes
-		////create output file name and stream 
-		
-		//create symbolTable 
-		
+		//Creating symbolTable 
 	}
-	//create output file name and stream
+	//Creating output file name and stream
 	Parser test= new Parser ("add.asm");
 	
-	
-	// TODO: march through the source code without generating any code
-		//for each label declaration (LABEL) that appears in the source code,
-		// add the pair <LABEL, n> to the symbol table
-		// n = romAddress which you should keep track of as you go through each line
-	//HINT: when should rom address increase? What kind of commands?
+	/*Marching through the source code without generating any code
+	for each label declaration (LABEL) that appears in the source code,
+	I will add the pair <LABEL, n> to the symbol table
+	n = romAddress which I will keep track of as I go through each line*/
 	private static void firstPass(String inputFileName, SymbolTable symbolTable) {
 
 	}
 	
-	// TODO: march again through the source code and process each line:
-		// if the line is a c-instruction, simple (translate)
-		// if the line is @xxx where xxx is a number, simple (translate)
-		// if the line is @xxx and xxx is a symbol, look it up in the symbol
-		//	table and proceed as follows:
-			// If the symbol is found, replace it with its numeric value and
-			//	and complete the commands translation
-			// If the symbol is not found, then it must represent a new variable:
-				// add the pair <xxx, n> to the symbol table, where n is the next
-				//	available RAM address, and complete the commands translation
-	// HINT: when should rom address increase?  What should ram address start
-	// at? When should it increase?  What do you do with L commands and No commands?
+	/*Marching again through the source code and process each line:
+	if the line is a c-instruction I will simple (translate)
+	if the line is @xxx where xxx is a number, I will simple (translate)
+	if the line is @xxx and xxx is a symbol, I will look it up in the symbol
+	table Then:
+		If the symbol is found, I will replace it with its numeric value and I will complete the commands translation
+		If the symbol is not found, then it must represent a new variable:
+		I will add the pair <xxx, n> to the symbol table, where n is the next available RAM address, and will complete the commands translation */
 	private static void secondPass(String inputFileName, SymbolTable symbolTable, PrintWriter outputFile) {
-
+		
 	}
-	
-
 
 }
